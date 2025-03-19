d7070dada2647928941fec4a9e417350992b0926
Kira Chen <chenki@shernet.sheridancollege.ca>
2025-03-19 10:15:36 -0400

    clock minute and hour hands move, button stops the clock from moving

---
+++ Assets/Scripts/KitClock.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class KitClock : MonoBehaviour
+{
+    public Transform hourHand;
+    public Transform minuteHand;
+    public float timeAnHourTakes = 5;
+
+    public float t;
+    public int hour = 0;
+
+    public UnityEvent<int> OnTheHour;
+
+    Coroutine clockIsRunning;
+    IEnumerator doingOneHourOfMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        clockIsRunning = StartCoroutine(MoveTheClock());
+    }
+
+    IEnumerator MoveTheClock()
+    {
+        while (true)
+        {
+            doingOneHourOfMovement = MoveTheClockHandsOneHour();
+            yield return StartCoroutine(doingOneHourOfMovement);
+        }
+        
+    }
+
+    // Update is called once per frame
+    IEnumerator MoveTheClockHandsOneHour()
+    {
+        t = 0;
+        while (t < timeAnHourTakes)
+        {
+            t += Time.deltaTime;
+            minuteHand.Rotate(0, 0, -(360 / timeAnHourTakes) * Time.deltaTime);
+            hourHand.Rotate(0, 0, -(30 / timeAnHourTakes) * Time.deltaTime);
+            yield return null;
+        }
+        hour++;
+        if(hour == 13)
+        {
+            hour = 1;
+        }
+
+        OnTheHour.Invoke(hour);
+    }
+
+    public void StopTheClock()
+    {
+        StopCoroutine(clockIsRunning);
+        StopCoroutine(doingOneHourOfMovement);
+    }
+
+}
+    
